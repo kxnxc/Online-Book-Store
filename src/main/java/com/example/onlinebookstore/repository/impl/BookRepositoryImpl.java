62e91aa0a4b3c17003cc1fd7dc8aba94792ca003
Yehor Vitvytskyi <grey5393@gmail.com>
2023-12-17 07:24:11 +0200

    implemented book model, repository and service

---
+++ src/main/java/com/example/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.example.onlinebookstore.repository.impl;
+
+import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.repository.BookRepository;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                throw new RuntimeException("Can't create book" + book, e);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> getOrder = session.createQuery("FROM Book ", Book.class);
+            return getOrder.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Error. No books found", e);
+        }
+    }
+}
