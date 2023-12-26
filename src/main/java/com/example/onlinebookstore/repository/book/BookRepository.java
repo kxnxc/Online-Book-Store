9b03f9859d4ffb3bad023c0d4300226fb4fcec54
kxnxc <113771672+kxnxc@users.noreply.github.com>
2023-12-26 11:31:38 +0200

    added CustomGlobalExceptionHandler class, validation for CreateBookRe… (#5)
    * added CustomGlobalExceptionHandler class, validation for CreateBookRequestDto, UniqueIsbn annotation for validation
    
    * changed UniqueIsbn annotation to ISBN annotation from hibernate.validator

--- src/main/java/com/example/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/book/BookRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     Optional<Book> findById(Long id);
+
+    Optional<Book> findByIsbn(String isbn);
 }
