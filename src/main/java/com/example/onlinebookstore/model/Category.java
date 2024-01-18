338e865337a925ec1474214f0abcf007099d07de
Yehor Vitvytskyi <grey5393@gmail.com>
2024-01-18 14:20:17 +0200

    fixed after review

--- src/main/java/com/example/onlinebookstore/model/Category.java
+++ src/main/java/com/example/onlinebookstore/model/Category.java
@@ -12,8 +12,8 @@ import org.hibernate.annotations.Where;
 
 @Data
 @Entity
-@SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
-@Where(clause = "is_deleted=false")
+@SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = FALSE")
 @Table(name = "categories")
 public class Category {
     @Id
