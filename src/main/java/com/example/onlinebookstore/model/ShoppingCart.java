7634d908b7a1a8989699a8171dea3181558fbb05
Yehor Vitvytskyi <grey5393@gmail.com>
2024-01-18 21:18:22 +0200

    fixed shopping cart, cart item models and tables

--- src/main/java/com/example/onlinebookstore/model/ShoppingCart.java
+++ src/main/java/com/example/onlinebookstore/model/ShoppingCart.java
@@ -13,11 +13,13 @@ import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@NoArgsConstructor
 @SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id = ?")
 @Where(clause = "is_deleted = FALSE")
 @Table(name = "shopping_carts")
